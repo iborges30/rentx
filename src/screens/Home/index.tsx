7bf7eb57702daccda1da3c15126a0c5850be4576
unknown <clickwebdesignertga@gmail.com>
2022-03-31 18:12:53 -0400

    card

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -3,10 +3,33 @@ import { StatusBar } from "react-native";
 import { RFValue } from "react-native-responsive-fontsize";
 
 import Logo from "../../assets/logo.svg";
+import { Car } from "../../components/Car";
 
 import { Container, Header, TotalCars,HeaderContent } from "./styles";
 
 export function Home() {
+  const carData={
+    brand:'Audi',
+    name:'Coupé',
+    rent:{
+      period:'AO DIA',
+      price:120
+    },
+    thumbnail:'https://mediaservice.audi.com/media/live/50900/fly1400x601n1/f5f/2021.png'
+  }
+
+
+  const carDatatwo={
+    brand:'Audi',
+    name:'Coupé',
+    rent:{
+      period:'AO DIA',
+      price:120
+    },
+    thumbnail:'https://mediaservice.audi.com/media/live/50900/fly1400x601n1/f83rh7/2022.png'
+  }
+
+
   return (
     <Container>
       <StatusBar
@@ -21,6 +44,9 @@ export function Home() {
           <TotalCars> Total de 12 carros</TotalCars>
         </HeaderContent>
       </Header>
+
+      <Car data={carData}/>
+      <Car data={carDatatwo}/>
     </Container>
   );
 }
